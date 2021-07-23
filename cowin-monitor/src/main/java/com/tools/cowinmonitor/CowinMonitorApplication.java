a98819991ae61952708e4a2d70fc775fb1ac4181
ss354d <ss354d@att.com>
2021-07-23 18:58:06 +0530

    Initial commit with Spring boot inilialized components

---
+++ cowin-monitor/src/main/java/com/tools/cowinmonitor/CowinMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.tools.cowinmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowinMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CowinMonitorApplication.class, args);
+	}
+
+}
---
+++ cowin-monitor/src/test/java/com/tools/cowinmonitor/CowinMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tools.cowinmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowinMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
